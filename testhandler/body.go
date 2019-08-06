8b07fe43de2fc99627d51b7761281bec7abe89c5
asamusev <asamusev@ozon.ru>
2019-08-06 09:46:22 +0300

    add check body by request

---
+++ testhandler/body.go
@@ -0,0 +1,17 @@
+package testhandler
+
+import (
+	"io/ioutil"
+	"net/http"
+)
+
+// Body check json body request
+type Body string
+
+func (j Body) CanHandle(r *http.Request) bool {
+	if body, e := r.GetBody(); e == nil && body != nil {
+		b, err := ioutil.ReadAll(body)
+		return err == nil && string(b) == string(j)
+	}
+	return false
+}
