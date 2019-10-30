ce5384918cab3e8d66603a1a79c905c58b2b74c5
asamusev <asamusev@ozon.ru>
2019-10-30 10:17:33 +0300

    add doc

---
+++ doc.go
@@ -0,0 +1,7 @@
+/*
+Package httpclient for the helper based http client.
+[![Build Status](https://travis-ci.org/go-4devs/httpclient.svg?branch=master)](https://travis-ci.org/go-4devs/httpclient)
+[![Go Report Card](https://goreportcard.com/badge/github.com/go-4devs/httpclient)](https://goreportcard.com/report/github.com/go-4devs/httpclient)
+[![Coverage Status](https://coveralls.io/repos/github/go-4devs/httpclient/badge.svg?branch=master)](https://coveralls.io/github/go-4devs/httpclient?branch=master)
+*/
+package httpclient //import "github.com/go-4devs/httpclient"
