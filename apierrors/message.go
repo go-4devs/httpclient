b1267bb6844062c11371de066c5e49289202fd53
asamusev <asamusev@ozon.ru>
2019-10-30 11:58:20 +0300

    add staus code error message

--- apierrors/message.go
+++ apierrors/message.go
@@ -1,15 +1,29 @@
 package apierrors
 
+import (
+	"fmt"
+	"net/http"
+)
+
 // Message api error with message
 type Message struct {
-	Message string `json:"message"`
+	Message    string `json:"message"`
+	StatusCode int    `json:"status_code"`
 }
 
 func (m *Message) Error() string {
-	return m.Message
+	return fmt.Sprintf("StatusCode:%d, Message: %s", m.StatusCode, m.Message)
 }
 
 // ErrorMessage create error
+// deprecated: use HTTPErrorMessage
 func ErrorMessage() error {
 	return &Message{}
+}
+
+// HTTPErrorMessage create error with status code
+func HTTPErrorMessage(r *http.Response) error {
+	return &Message{
+		StatusCode: r.StatusCode,
+	}
 }
